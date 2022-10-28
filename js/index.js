d778327622216c05ccbf42e679ffb2df2fc1e938
Biman Kumar Das <daskumarbiman2020@gmail.com>
2022-10-28 10:30:00 +0530

    update5

--- js/index.js
+++ js/index.js
@@ -136,20 +136,6 @@ window.onload=function(){
 
 
 
-// document.onreadystatechange = function() {
-//   if (document.readyState !== "complete") {
-//       document.querySelector(
-//         "body").style.visibility = "hidden";
-//       document.querySelector(
-//         "#spinner").style.visibility = "visible";
-//   } else {
-//       document.querySelector(
-//         "#spinner").style.display = "none";
-//       document.querySelector(
-//         "body").style.visibility = "visible";
-//   }
-// };
-
 
 
 
